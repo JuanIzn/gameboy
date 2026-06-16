4eda12755a6c9164ba17d40eaaca5ed2415dd745
Juan Jesus <alu.142910@usj.es>
2026-06-16 16:13:45 +0200

    document cart code

--- src/include/cart.h
+++ src/include/cart.h
@@ -1,26 +1,35 @@
+#pragma once
+
 #include "common.h"
 
-#pragma once
-/** 
- *  We will define the cart memory structure
- */
+// Exact layout of the cartridge header in ROM (0x0100 - 0x014F)
+// Ref: https://gbdev.io/pandocs/The_Cartridge_Header.html
 typedef struct {
-    u8 entry[4];
-    u8 nintendo_logo[48]; 
-    u8 title[16]; 
-    size_t rom_size; 
-    u8 version[];
-
-    u8 checksum[];
-} rom_header;
+    u8 entry[4];           // 0x0100 - 0x0103  entry point
+    u8 nintendo_logo[48];  // 0x0104 - 0x0133  validated by boot ROM
+    u8 title[16];          // 0x0134 - 0x0143  game title in ASCII
+    u8 new_lic_code[2];    // 0x0144 - 0x0145
+    u8 sgb_flag;           // 0x0146
+    u8 type;               // 0x0147  cartridge MBC type
+    u8 rom_size;           // 0x0148  code: 0x00=32KB, 0x01=64KB...
+    u8 ram_size;           // 0x0149
+    u8 dest_code;          // 0x014A
+    u8 lic_code;           // 0x014B
+    u8 version;            // 0x014C
+    u8 checksum;           // 0x014D  verified on startup
+    u8 global_checksum[2]; // 0x014E - 0x014F
+} __attribute__((packed)) rom_header;
 
 typedef struct {
-    u8 *rom_data;
-    u32 *rom_size
-    char *filename;
-
-    rom_header *header;
+    char      *filename;
+    u8        *rom_data;  // full contents of the .gb file
+    u32        rom_size;  // actual file size in bytes
+    rom_header *header;   // points to rom_data + 0x0100
 } cart_context;
 
-void load_cart();
-void read_cart();
+// Loads the .gb file into memory and validates the header.
+// Returns true if the cartridge is valid.
+bool cart_load(char *path);
+
+// Reads a byte from the cartridge given a memory bus address.
+u8 cart_read(u16 address);
